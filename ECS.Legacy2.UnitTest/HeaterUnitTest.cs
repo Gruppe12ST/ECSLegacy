99ab0b2fe13068e26e225c2ea0fcca7a9e2dd48c
Lærke Husted <201809413@post.au.dk>
2020-02-12 15:41:15 +0100

    test

---
+++ ECS.Legacy2.UnitTest/HeaterUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy2.UnitTest
+{
+    [TestFixture]
+
+    class HeaterUnitTest
+    {
+        private IHeater heater;
+        private FakeLogger fake;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+    }
+
+    public class FakeLogger : ILogger
+    {
+        public int value = 0;
+
+        public void PrintOn()
+        {
+            value = 1;
+        }
+
+        public void PrintOff()
+        {
+            value = 2;
+        }
+    }
+}
